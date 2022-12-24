a64a3f97b552c9aa125256906e7953f079856cf9
Prashant <prashantpokhrel15@gmail.com>
2022-12-25 00:15:04 +1100

    header section complete

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+
+import { images } from "../../constants";
 import "./subheading.css";
-const SubHeading = () => {
-  return <div>SubHeading</div>;
+const SubHeading = ({ title }) => {
+  return (
+    <div style={{ marginBottom: "1rem" }}>
+      <p className="p__cormorant">{title}</p>
+      <img src={images.spoon} alt="spoon" className="spoon__img" />
+    </div>
+  );
 };
 
 export default SubHeading;
