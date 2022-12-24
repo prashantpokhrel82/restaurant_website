81e4b44c4dc073b0f9186f19858906a446ee2222
Prashant <prashantpokhrel15@gmail.com>
2022-12-24 18:19:58 +1100

    app layout folder structuring

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Footer from "./Footer/Footer";
+import MenuItem from "./MenuItem/MenuItem";
+import NavBar from "./NavBar/NavBar";
+import SubHeading from "./SubHeading/SubHeading";
+export { Footer, MenuItem, NavBar, SubHeading };
---
+++ src/container/Laurels/Laurels.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./laurels.css";
+const Laurels = () => {
+  return <div>Laurels</div>;
+};
+
+export default Laurels;
